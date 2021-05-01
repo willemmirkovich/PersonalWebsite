99fadcf7b16553d711cd8e50c0de9601b14b80cf
Willem Mirkovich <willemmirkovich@Willems-MBP.hsd1.co.comcast.net>
2021-05-01 00:12:43 -0600

    react added

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-// import { Container,  } from "react-bootstrap";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Header from "./header";
 import Footer from "./footer";
