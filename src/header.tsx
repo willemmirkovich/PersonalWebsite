3fa7abce3d812ab9ea606bb9a9bbcac1a4cec086
Willem Mirkovich <willemmirkovich@Willems-MBP.hsd1.co.comcast.net>
2021-04-30 23:58:49 -0600

    addition of footer

--- src/header.tsx
+++ src/header.tsx
@@ -3,11 +3,14 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return(
-        <nav>
-            <NavLink exact activeClassName='active' to="/">Home</NavLink>
-            <NavLink exact activeClassName='active' to="/experience">Experience</NavLink>
-            <NavLink exact activeClassName='active' to="/resume">Resume</NavLink>
-        </nav>
+        // TODO: determine if best way to do this
+        <header>
+            <nav>
+                <NavLink exact activeClassName='active' to="/">Home</NavLink>
+                <NavLink exact activeClassName='active' to="/experience">Experience</NavLink>
+                <NavLink exact activeClassName='active' to="/resume">Resume</NavLink>
+            </nav>
+        </header>
     )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 // import { Container,  } from "react-bootstrap";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Header from "./header";
+import Footer from "./footer";
 import Experience from "./experience";
 
 const Main = () => {
@@ -19,6 +20,7 @@ const router = (
                 <Route exact path="/" component={Main}></Route>
                 <Route path="/experience" component={Experience}></Route>
             </Switch>
+            <Footer />
         </div>
     </Router>
 )
