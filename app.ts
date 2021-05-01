d973719e7c26fb1d781baa2303d181d2d7f8a204
Willem Mirkovich <willemmirkovich@Willems-MBP.hsd1.co.comcast.net>
2021-04-30 22:41:10 -0600

    basic react functionality

--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import { render } from "react-dom";
 
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.use(express.static(__dirname)); // TODO: modify
+
+app.get('/js', (req, res) => res.sendFile(`${__dirname}/dist/main.js`));
+
+app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+
+const main = () => {
+    return (
+        <h1>Hello</h1>
+    )
+}
+
+render(main(), document.getElementById('root'));
